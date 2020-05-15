a02c3678f70cc9d243c250874840f92de155a71c
bhavay <bhavaypuri15@gmail.com>
2020-05-15 15:45:20 +0530

    Production failure

--- editor-backend/Routes/codeRun.js
+++ editor-backend/Routes/codeRun.js
@@ -2,7 +2,12 @@ const router = require('express').Router();
 const fs = require('fs');
 const { exec } = require('child_process');
 
+router.get('/',(req, res) => {
+    console.log('here');
+    res.send('ok');
+})
 router.post('/run', (req, res) => {
+    console.log('run');
     //req.body items
     let code = req.body.code;
     let input = req.body.input;
