f9a93be5ff4ffdf66be9da6ced1a4b76a7e50c6f
bhavay <bhavaypuri15@gmail.com>
2020-05-22 02:24:01 +0530

    Made Editor Componenet

--- editor/src/Components/EditorComponent.js
+++ editor/src/Components/EditorComponent.js
@@ -1,31 +1,33 @@
 import React from 'react';
-import { Row, Col, Input } from 'antd';
+import MonacoEditor from 'react-monaco-editor';
+import {Input} from 'antd';
+const {TextArea} = Input;
 
 const EditorComponent = props => {
-    const codes = props.codes;
+    const {code,input,output,editorDidMount, editorOnChange, handleRun, handleInput} = props;
+    const options = {
+        selectOnLineNumbers: true, // Select line by clicking on line number
+        minimap: {
+            enabled: false // Minimap gives an overview of code(present on right side in vscode) 
+        }
+    }
     return (
         <div>
-            <Row>
-                <Col span={16}>
-                    {
-                        codes.map((code, idx) => (
-                            <Input
-                                key={idx}
-                                type="text"
-                                value={code}
-                                size="small"
-                                addonBefore={idx + 1}
-                                onChange={e => props.handleChange(e, idx)}
-                                onKeyDown={e => props.handleKeyDown(e, idx)}
-                                onKeyUp={e => props.handleKeyUp(e, idx)}
-                            />
-                        ))
-                    }
-                </Col>
-                <Col span={8}>
-                </Col>
-            </Row>
-            <button type="button" style={{ marginTop: '10px' }} onClick={props.handleRun}> run code</button>
+            <MonacoEditor
+                width="600"
+                height="400"
+                language="cpp"
+                theme="vs-dark"
+                value={code}
+                options={options}
+                editorDidMount={editorDidMount}
+                onChange={editorOnChange}
+            />
+            <button onClick={handleRun}>run code</button>
+            <div>input:</div>
+            <TextArea value={input} onChange={handleInput} />
+            <div>output:</div>
+            <TextArea value={output} style={{marginBottom: '20px'}} autoSize={{minRows:2, maxRows: 5}} readOnly={true} />
         </div>
     );
 }
--- editor/src/Containers/Editor.js
+++ editor/src/Containers/Editor.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import EditorComponent from '../Components/EditorComponent';
 import axios from 'axios';
-import MonacoEditor from 'react-monaco-editor';
 
 // automatically reconnects if the connection is closed
 import ReconnectingWebSocket from 'reconnecting-websocket';
@@ -37,76 +36,58 @@ class Editor extends Component {
             if (err) throw err;
             var binding = new StringBinding(this.state.editor, this, doc, ['content']);
             binding.setup(this);
-            this.setState({binding});
+            this.setState({ binding });
         });
     }
 
     editorDidMount = (editor, monaco) => {
         editor.focus();
         // editor.setSelection(new monaco.Range(1,1,1,1));
-        this.setState({editor, monaco})
+        this.setState({ editor, monaco })
     }
 
-    onChange = (newValue, e) => {
+    // Monaco editor onChange()
+    editorOnChange = (newValue, e) => {
         this.state.binding._inputListener(this.state.code, newValue, e);
     }
 
+    // Handler for Run Code button
     handleRun = () => {
         // Convert array of codes into a single string
         const code = this.state.editor.getValue();
         // Send API call to run code
-        axios.post(serverURL+'/code/run', {
+        axios.post(serverURL + '/code/run', {
             code: code,
             input: this.state.input,
             id: 123
         }).then(response => {
             this.state.binding._outListener(this.state.output, response.data);
-            this.setState({output: response.data});
-            // doc.submitOp([{ p: ['output',0], ld: this.state.output, li: response.data }]); //p: PATH
-            
+            this.setState({ output: response.data });
+
         }).catch(err => {
-            if(err.response.status === 400) {
+            if (err.response.status === 400) {
                 this.state.binding._outListener(this.state.output, err.response.data);
-                this.setState({output: err.response.data});
+                this.setState({ output: err.response.data });
             }
-            
-            // doc.submitOp([{ p: ['output',0], ld: this.state.output, li: String(err.response.data) }]); //p: PATH
         })
     }
 
-    handleInputChange = (e) => {
+    handleInput = (e) => {
         this.state.binding._inListener(this.state.input, e.target.value);
-        this.setState({input: e.target.value});
+        this.setState({ input: e.target.value });
     }
 
     render() {
-        const code = this.state.code;
-        const input = this.state.input;
-        const output = this.state.output;
-        const options = {
-            selectOnLineNumbers: true, // Select line by clicking on line number
-            minimap: {
-                enabled: false
-            }
-        }
         return (
-            <React.Fragment>
-            <MonacoEditor
-            width="600"
-            height="400"
-            language="cpp"
-            theme="vs-dark"
-            value={code}
-            options={options}
-            onChange={this.onChange}
-            editorDidMount={this.editorDidMount}
+            <EditorComponent
+                code={this.state.code}
+                input={this.state.input}
+                output={this.state.output}
+                editorDidMount={this.editorDidMount}
+                onChange={this.editorOnChange}
+                handleRun={this.handleRun}
+                handleInput={this.handleInput}
             />
-            <button onClick={this.handleRun}>run code</button>
-            <div>input:</div>
-            <textarea value={input} onChange={this.handleInputChange}></textarea>
-            <div>output:</div>
-            <textarea value={output} readOnly={true}></textarea>
-            </React.Fragment>
         );
     }
 }
