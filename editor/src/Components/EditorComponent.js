0583f6685eb10741b6bd7e12ba79c95fc5a5f165
bhavay <bhavaypuri15@gmail.com>
2020-06-11 12:25:07 +0530

    Change layout

--- editor/src/Components/EditorComponent.js
+++ editor/src/Components/EditorComponent.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import MonacoEditor from 'react-monaco-editor';
-import {Input, Select, Button} from 'antd';
-const {TextArea} = Input;
-const {Option} = Select;
+import { Input, Select, Row, Col } from 'antd';
+const { TextArea } = Input;
+const { Option } = Select;
 
 const EditorComponent = props => {
-    const {code,input,output,editorDidMount, editorOnChange, handleRun, handleInput, handleLang, lang} = props;
+    const { code, input, output, editorDidMount, editorOnChange, handleRun, handleInput, handleLang, lang } = props;
     const options = {
         selectOnLineNumbers: true, // Select line by clicking on line number
         minimap: {
@@ -13,27 +13,31 @@ const EditorComponent = props => {
         }
     }
     return (
-        <div>
-            <MonacoEditor
-                width="600"
-                height="400"
-                language={lang}
-                theme="vs-dark"
-                value={code}
-                options={options}
-                editorDidMount={editorDidMount}
-                onChange={editorOnChange}
-            />
-            <Select defaultValue='cpp' onChange={handleLang} value={lang}>
-                <Option value='cpp'>CPP</Option>
-                <Option value='java'>Java</Option>
-            </Select>
-            <button onClick={handleRun}>run code</button>
-            <div>input:</div>
-            <TextArea value={input} onChange={handleInput} />
-            <div>output:</div>
-            <TextArea value={output} style={{marginBottom: '20px'}} autoSize={{minRows:2, maxRows: 5}} readOnly={true} />
-        </div>
+        <Row>
+            <Col span={12}>
+                <MonacoEditor
+                    width="600"
+                    height="400"
+                    language={lang}
+                    theme="vs-dark"
+                    value={code}
+                    options={options}
+                    editorDidMount={editorDidMount}
+                    onChange={editorOnChange}
+                />
+                <Select defaultValue='cpp' onChange={handleLang} value={lang}>
+                    <Option value='cpp'>CPP</Option>
+                    <Option value='java'>Java</Option>
+                </Select>
+                <button onClick={handleRun}>run code</button>
+            </Col>
+            <Col span={12}>
+                <div>input:</div>
+                <TextArea value={input} onChange={handleInput} rows={5} />
+                <div>output:</div>
+                <TextArea value={output} style={{ marginBottom: '20px' }} autoSize={{ minRows: 5, maxRows: 10 }} readOnly={true} />
+            </Col>
+        </Row>
     );
 }
 
